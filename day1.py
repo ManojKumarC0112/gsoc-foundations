7e675fb92c737f471b2bedaf9d1977866233f941
ManojKumarC0112 <manojkchandrashekar@gmail.com>
2026-01-10 18:07:31 +0530

    day 1 task completed

---
+++ day1.py
@@ -0,0 +1,17 @@
+#task 1
+
+# name=input("Enter your name: ")
+# age=int(input("enter your age:"))
+# print("your name is",name,"and your age next year is",age+1,)
+
+#taske 1.2
+print("using while loop")
+i=1
+while(i<=10):
+    print(i,end=" ")
+    i=i+1
+print("")
+print("using for loop")
+for i in range(1,11):
+    print(i,end=" ")
+
