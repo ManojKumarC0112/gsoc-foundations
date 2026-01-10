bfa4f789bcc9977605c43aded57d539ce90cfc71
ManojKumarC0112 <manojkchandrashekar@gmail.com>
2026-01-10 18:19:34 +0530

    day1 task 3 completed

--- day1.py
+++ day1.py
@@ -5,13 +5,25 @@
 # print("your name is",name,"and your age next year is",age+1,)
 
 #taske 1.2
-print("using while loop")
-i=1
-while(i<=10):
-    print(i,end=" ")
-    i=i+1
-print("")
-print("using for loop")
-for i in range(1,11):
-    print(i,end=" ")
+# print("using while loop")
+# i=1
+# while(i<=10):
+#     print(i,end=" ")
+#     i=i+1
+# print("")
+# print("using for loop")
+# for i in range(1,11):
+#     print(i,end=" ")
 
+#task 3
+numbers=[1,2,2,3,3,3,4]
+
+freq={}
+
+for n in numbers:
+    if n in freq:
+        freq[n]=freq[n]+1
+    else:
+        freq[n]=1
+
+print(freq)
