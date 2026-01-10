4fa34042ce60071b34e5bffedbadfb7289a6ff29
ManojKumarC0112 <manojkchandrashekar@gmail.com>
2026-01-10 18:52:03 +0530

    day3 errorsexceptions debugging

--- daily-log.md
+++ daily-log.md
@@ -7,3 +7,6 @@ i worked
 day2
 
 i worked on how to return a string and count even numbers in function open write and read a file the hardest was the file task i got confused with the sysntax 
+
+day 3
+i worked on errors and types of errors i worked on try and execept methods and also i did a grading system using if and elif and else and also i correct a buggy code with index out of range
---
+++ day3.py
@@ -0,0 +1,28 @@
+#task 1
+#print("Age"+20)
+#int("abc")
+#x=10/0
+
+#task 2 try/except
+# try:
+#     age=int(input("Enter your age: "))
+#     print(age)
+# except ValueError:
+#     print("You did not enter a valid number")
+
+#task 3
+# marks=int(input("Enter marks: "))
+# if marks >= 90:
+#     print("A")
+# elif marks >= 75:
+#     print("B")
+# elif marks >= 60:
+#     print("C")
+# else:
+#     print("Fail")
+
+#task 4
+numbers = [1, 2, 3, 4, 5]
+
+for i in range(len(numbers)):
+    print(numbers[i])
