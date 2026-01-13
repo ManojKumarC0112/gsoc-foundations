de48caef03b7d99002f75fab1b521944a3617a8c
ManojKumarC0112 <manojkchandrashekar@gmail.com>
2026-01-13 23:55:47 +0530

    day 10 got to know about GET and POST methods and how to use add data from url

--- app.py
+++ app.py
@@ -2,21 +2,47 @@ from flask import *
 import json
 
 app=Flask(__name__)
-
-@app.route('/students/<int:student_id>')
-def getstudents(student_id):
-    if student_id==101:
-        return jsonify({
-            'id':101,
-            'name':'manoj',
-             'marks':99
-        })
-    else:
+students={
+    '101':{
+        'name':'manoj',
+    }
+}
+@app.route('/')
+def home():
+    return "dont worry working well"
+@app.route("/students/<int:student_id>",methods=['GET'])
+def get_students(student_id):
+    if student_id not in students:
         return jsonify(
             {
                 'error':'Student not found'
             }
         ),404
+    else:
+        return jsonify(students[student_id])
+
+@app.route("/students",methods=['POST'])
+def add_students():
+    data=request.get_json()
+
+    if data is None:
+        return jsonify({"error":"No data"}),404
+
+    if "id" not in data or "name" not in data or "marks" not in data:
+        return jsonify({'error':'missing fileds'}),404
+
+    student_id=data['id']
+
+    if student_id in students:
+        return jsonify({'error':'Student already exists'}),400
+
+    students[student_id]={
+        'id':student_id,
+        "name":data['name'],
+        "marks":data['marks']
+    }
+    return jsonify({"message":"Student added successfully"}),200
+
 
 if __name__=='__main__':
     app.run(debug=True)
